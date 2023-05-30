daa9db0bc8b1d11632550063d8a1bbc45b76917e
Dan Berezin <Bogdanlinee@gmail.com>
2023-05-30 21:42:30 +0300

    Basic routes and controllers

--- app.js
+++ app.js
@@ -1,30 +1,37 @@
 require('dotenv').config();
 require('express-async-errors');
+require('dotenv');
 const express = require('express');
 const app = express();
-
-
-//new comment for the test
 
 // error handler
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+// DB connection
+const connectDB = require('./db/connect.js');
+
+// auth middleware
+const authMiddleware = require('./routes/auth.js');
+const jobMiddleware = require('./routes/jobs.js');
+
 app.use(express.json());
-// extra packages
+app.use('/api/v1/auth', authMiddleware);
+app.use('/api/v1/jobs', jobMiddleware);
 
 // routes
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.send('jobs api');
 });
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URL);
     app.listen(port, () =>
       console.log(`Server is listening on port ${port}...`)
     );
