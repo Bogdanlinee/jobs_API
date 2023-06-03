9512755be93b2c727644272d5eebd2f82282d4cd
Dan Berezin <Bogdanlinee@gmail.com>
2023-06-03 11:50:18 +0300

    Changed the Jobs controllers

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,18 +1,22 @@
 const JobSchema = require('../models/Job.js');
 const { StatusCodes } = require('http-status-codes');
+const { BadRequestError, UnauthenticatedError } = require('../errors/index.js');
 
 const getJob = async (req, res) => {
   res.status(StatusCodes.CREATED).json({ created: 'getJob' });
 }
 
 const getAllJobs = async (req, res) => {
-  res.status(StatusCodes.CREATED).json({ created: 'getAllJobs' });
+  const jobs = await JobSchema.find({ createdBy: req.user.userId })
+    .sort('-createdAt')
+    .select('status company position createdAt -_id');
+  res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 }
 
 const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
   const job = await JobSchema.create(req.body);
-  console.log(job);
-  res.status(StatusCodes.CREATED).json(req.user);
+  res.status(StatusCodes.CREATED).json(job);
 };
 
 const changeJob = async (req, res) => {
