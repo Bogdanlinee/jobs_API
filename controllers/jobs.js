bcc5e1fd9bae5aa083035174811ae126d8edbe33
Dan Berezin <Bogdanlinee@gmail.com>
2023-06-06 20:57:23 +0300

    Set up cintrollers for job middleware

--- controllers/jobs.js
+++ controllers/jobs.js
@@ -1,15 +1,24 @@
 const JobSchema = require('../models/Job.js');
 const { StatusCodes } = require('http-status-codes');
-const { BadRequestError, UnauthenticatedError } = require('../errors/index.js');
+const { BadRequestError, UnauthenticatedError, NotFoundError } = require('../errors/index.js');
 
 const getJob = async (req, res) => {
-  res.status(StatusCodes.CREATED).json({ created: 'getJob' });
+  const { id: jobId } = req.params;
+  const { userId } = req.user;
+  const job = await JobSchema.findOne({ createdBy: userId, _id: jobId })
+    .sort('-createdAt')
+    .select('status company position createdAt');
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 }
 
 const getAllJobs = async (req, res) => {
   const jobs = await JobSchema.find({ createdBy: req.user.userId })
     .sort('-createdAt')
-    .select('status company position createdAt -_id');
+    .select('status company position createdAt');
   res.status(StatusCodes.OK).json({ jobs, count: jobs.length });
 }
 
@@ -20,11 +29,38 @@ const createJob = async (req, res) => {
 };
 
 const changeJob = async (req, res) => {
-  res.status(StatusCodes.CREATED).json({ created: 'changeJob' });
+  const {
+    params: { id: jobId },
+    user: { userId },
+    body: { position, company }
+  } = req;
+
+  if (!position.trim() || !position.trim()) {
+    throw new BadRequestError('Please provide a new position.');
+  }
+
+  const job = await JobSchema.findOneAndUpdate(
+    { createdBy: userId, _id: jobId },
+    { position, company },
+    { returnDocument: 'after', runValidators: true }
+  ).select('status company position createdAt');
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const deleteJob = async (req, res) => {
-  res.status(StatusCodes.CREATED).json({ created: 'deleteJob' });
+  const { id: jobId } = req.params;
+  const { userId } = req.user;
+  const job = await JobSchema.findOneAndDelete({ createdBy: userId, _id: jobId });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).send();
 };
 
 module.exports = { getJob, getAllJobs, createJob, changeJob, deleteJob };
